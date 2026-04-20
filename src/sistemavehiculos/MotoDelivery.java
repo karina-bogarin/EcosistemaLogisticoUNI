bac8eeaf0732fddffc881733bf9c4fad2ae095d1
kevingimenez-collab <kevin.gimenez@fiuni.edu.py>
2026-04-19 22:57:39 -0300

    Create MotoDelivery.java

---
+++ src/sistemavehiculos/MotoDelivery.java
@@ -0,0 +1,42 @@
+/**
+ * Esta clase hereda de {@link Vehiculo} e implementa {@link IConectable}.
+ * Se trata de una moto utilizada para realizar servicios de delivery.
+ * 
+ * @author Kevin Gimenez.
+ * @version 1.0
+ * @since 04/2026
+ */
+public class MotoDelivery extends Vehiculo implements IConectable{
+    private double combustible;
+    
+    /**
+     * Constructor de la clase.
+     * @param id : Identificador de la moto (vehiculo)
+     * @param combustible : Litros de combustible que posee la moto.
+     */
+    public MotoDelivery(int id, double combustible){
+        super(id);
+        this.combustible = combustible;
+    }
+    
+    // Sobreescritura de los metodos de Vehiculo y IConectable.
+    @Override
+    public void patronMovimiento(){
+        System.out.println("Moto " + getId() + " saliendo del garage...");
+    }
+    
+    @Override
+    public void sincronizarGPS(){
+        System.out.println("Sincronizando satelites para ubicar a la moto " + getId() + "...");
+    }
+    
+    @Override
+    public void ubicarVehiculo(){
+        System.out.println("Moto " + getId() + " se dirige hacia Ruta 1 a 85Km/h.");
+    }
+    
+    // Getter del combustible de la moto.
+    public double getCombustible(){
+        return combustible;
+    }
+}
